6f82944b011dc9c1ac2b6ee41404d35c9c8a722d
DaniloTorres2001 <dantvera@espol.edu.ec>
2021-10-30 17:59:14 -0500

    Primer commit de ventana public class Run {
    	public static void main(String[] args) {
    		// TODO Auto-generated method stub
    		Ventana iniciar = new Ventana ();
    		iniciar.setVisible(true);
    	}
    }

---
+++ src/Ventana.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Ventana extends JFrame {
+
+	public Ventana() {
+		// TODO Auto-generated constructor stub
+		setTitle("Prueba");
+		setSize(300,300);
+		setLocation(250,250);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
